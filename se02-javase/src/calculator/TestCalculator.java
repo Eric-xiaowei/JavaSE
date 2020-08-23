a99fb6d7f0f22165f2760a7e0550521ffa9f4b6d
ZhangXiaowei <2360728436@qq.com>
2020-08-23 11:38:52 +0800

    DO-UPDATE3

---
+++ se02-javase/src/calculator/TestCalculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+
+/**
+ * @author:ZhangXiaowei
+ * @create:2020-08-2020/8/23-11:25
+ */
+public class TestCalculator {
+
+}
--- se02-javase/src/value/TestValue.java
+++ se02-javase/src/value/TestValue.java
@@ -1,5 +1,7 @@
 package value;
 
+import java.util.Arrays;
+
 /**
  * @author:ZhangXiaowei
  * @create:2020-08-2020/8/23-10:32
@@ -15,6 +17,39 @@ public class TestValue {
         System.out.println(a);
 
         //2.浮点数类型的字面值错误：一般浮点数的字面值为double类型
-        //float b = 3.14;数值的字面值为double类型的，
+        //float b = 3.14;数值的字面值为double类型的，字面类型与实际类型不匹配
+        float b = 3.14F;
+        System.out.println(b);
+        double c = 3.14;
+        double d = 3.14F;
+        double e = 3;//字面值为整型的数据，但是double为大类型数据甚至比long类型都要大，因此小转大没问题。
+        System.out.println(c+"==="+d+"==="+e);
+
+        //3.关于char类型的数据：
+        char a1 = 65; //char类型的数据，可以直接存储一个数字，底层自动查询ASCII码表，超过的部分查询：Unicode表
+        //这里是封装了下面的过程，其实还是存在一个转型的过程。
+        System.out.println(a1);
+        char a2 = (char)65; //这里表示将字面值为整数的65，转化为char类型的数据，再到码表中进行查询。
+        System.out.println(a2);
+        //char b1 = 'cd'; char类型的数据只能存储一个字符的数据，超过一个就无法存储，超过一个的叫字符串，需要String类型
+        char a3 = '中';
+        System.out.println(a3);//char类型的数据是可以存储汉字的。0-65536，char类型默认字符集Unicode，包含汉字，固定长编码为2个字节
+
+        //4.使用char类型生成一个26个字母表：要求大写和小写各一份;
+        //4.1 生成大写字母表：
+        char[] letters = new char[26];
+        for(int i=65;i<65+letters.length;i++){
+            letters[i-65] = (char) i;//如果是数字的话，不用进行强转。
+        }
+        System.out.println(Arrays.toString(letters));//char类型的数组是可以直接进行打印的。
+
+        //4.2 生成小写的字母表：
+        char[] lowerLetter = new char[26];
+        for(int i=0;i<lowerLetter.length;i++){
+            lowerLetter[i] = (char) (i+97);
+        }
+        System.out.println(Arrays.toString(lowerLetter));
+
+
     }
 }
